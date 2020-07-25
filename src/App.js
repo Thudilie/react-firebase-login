35a0bf75de87d867e5aca30d80437447ce423b07
Thuy-Anh Nguyen <thuyanh.nguyen.de@gmail.com>
2020-07-25 16:03:54 +0200

    add firebase configurations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import firebase from "./Firebase";
 
 import Navigation from "./components/Navigation";
 import LandingLogged from "./components/LandingLogged";
@@ -8,10 +9,18 @@ import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    const ref = firebase.database().ref("user");
+    ref.on("value", (snapshot) => {
+      setLoggedIn(snapshot.val());
+    });
+  });
   return (
     <Router>
       <div className="App">
         <Navigation />
+        {loggedIn}
         <Switch>
           <Route exact path="/" component={LandingNotLogged} />
           <Route exact path="/" component={LandingLogged} />
